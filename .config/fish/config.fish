27807e13e233e809689fc478973bb042d7ec64d2
dixzz <vishusatyam36@gmail.com>
2026-06-17 00:25:08 +0530

    config bkp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 zoxide init fish | source
-direnv hook fish | source
-#fish_add_path /run/media/fed/DATA/flutter/bin
+#direnv hook fish | source
+fish_add_path /home/fed/Downloads/develop/flutter/bin
+set -x FLUTTER_ROOT /home/fed/Downloads/develop/flutter
 #set -x CHROME_EXECUTABLE /run/current-system/sw/bin/google-chrome-stable
